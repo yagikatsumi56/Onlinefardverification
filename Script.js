90a791ece7721b919e5045a22a311b3c87902c08
yagikatsumi56 <yagikatsumi56@gmail.com>
2026-06-06 13:55:03 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,6 +2,11 @@ async function populateFard() {
 
     const id = new URLSearchParams(window.location.search).get("id");
 
+    if (!id) {
+        document.body.innerHTML = "<h2>Invalid URL</h2>";
+        return;
+    }
+
     const res = await fetch(`data/${id}.json`);
 
     if (!res.ok) {
@@ -11,9 +16,9 @@ async function populateFard() {
 
     const fard = await res.json();
 
-    const PURPOSE_VALUE = {
+    const PURPOSE = {
         ElectricityConnection: "فرد برائے بجلی کنکشن",
-        PersonalRecord: "فرد برائے ذاتی ریکاڈ",
+        PersonalRecord: "فرد برائے ذاتی ریکاڈ"
     };
 
     const data = {
@@ -21,12 +26,19 @@ async function populateFard() {
         tehsil: fard.tehsil,
         place: fard.place,
         year: fard.year,
-        purpose: PURPOSE_VALUE[fard.purpose],
+        purpose: PURPOSE[fard.purpose] || fard.purpose,
+
         recordCenter: fard.recordCenter,
         fee: fard.fee,
+
         issuerFullName: `${fard.issuerFullName} ولد ${fard.issuerFatherName}`,
-        issueDate: new Date(fard.issueDate).toLocaleString(),
+
+        issueDate: fard.issueDate
+            ? new Date(fard.issueDate).toLocaleString()
+            : "-",
+
         token: fard.token,
+
         nameFName: `${fard.fullName} ${fard.fatherName}`,
         tribe: fard.tribe,
         cnic: fard.cnic
